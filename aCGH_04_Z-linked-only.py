ed05454a5f3956df8ffe076ea239bcf47fe73812
anicka-v <anna.volenikova@gmail.com>
2020-01-13 03:27:41 +0100

    Create aCGH_04_Z-linked-only.py

---
+++ aCGH_04_Z-linked-only.py
@@ -0,0 +1,34 @@
+#! /home/user/env python
+# coding: utf-8
+#
+##Script to print Z-linked genes
+#USAGE: python Z-linked_ONLY.py /ABS/PATH/TO/DIR/Infile_OK.txt
+#
+#1) PREPARE WORKSPACE
+import sys
+import os
+import statistics
+
+inputFile = sys.argv[1]
+species = input("Input abbreviation of your species: ")
+
+treshold = "0.5"
+
+with open(inputFile, 'r') as inFile, open((species + "_Z-linked_ONLY.txt"), 'w') as outFile:
+	for line in inFile:
+		if line.startswith("E"):
+			splitRow = line.split('\t')
+			geneID = splitRow[0] + '\n'
+			repValues = splitRow[1:]
+			Floats = [float(i) for i in repValues]
+			medianReps = statistics.median(Floats)
+			if medianReps > float(treshold):
+				outFile.write(geneID)
+			else:
+                        	continue
+inFile.close()
+outFile.close()
+
+print("Analysis done.")
+                
+
